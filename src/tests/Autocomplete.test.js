ea471bbf07d00d9c3087c3a9416e2fe8a02554c9
SuccessTracker <prosdev087@gmail.com>
2021-05-05 13:25:48 +0800

    Render test

--- src/tests/Autocomplete.test.js
+++ src/tests/Autocomplete.test.js
@@ -1,16 +1,27 @@
 import React from "react";
-import {render, screen} from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 
-import Autocomplete from "./Autocomplete";
+import Autocomplete from "../components/Autocomplete";
 
-jest.mock("./utils/api");
+jest.mock("../utils/api");
 
 describe("Autocomplete", () => {
-  it("renders correctly", () => {
-    render(<Autocomplete />);
+  let clickFn;
+  
+  const renderTest = () => {
+    clickFn = jest.fn()
+    const tools = render(<Autocomplete onClickProduct={clickFn} />)
 
-    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
-    
-    expect(input).toBeInTheDocument();
+    const input = screen.getByRole("textbox", {
+      placeholder: /search for a product/i,
+    });
+
+    return { input, tools }
+  }
+
+
+  it("renders correctly", () => {
+    renderTest()
+    screen.getByPlaceholderText('Search for a product')
   });
 });
