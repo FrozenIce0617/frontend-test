ca1e546cdfec56e530f65c991c048785f420cb12
SuccessTracker <prosdev087@gmail.com>
2021-05-05 12:55:17 +0800

    Handle product Id and pass to product detail

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState, useCallback } from "react"
 
-import "./App.css";
+import "./App.css"
 
-import Autocomplete from "./components/Autocomplete";
-import ProductDetail from "./components/ProductDetail";
+import Autocomplete from "./components/Autocomplete"
+import ProductDetail from "./components/ProductDetail"
 
 function App() {
+  const [productId, setProductId] = useState()
+
+  const onClickProduct = useCallback(
+    (id) => {
+      setProductId(id)
+    },
+    [setProductId]
+  );
+
   return (
-    <div className="App">
-      <Autocomplete />
-      <ProductDetail productId={null} />
+    <div className={productId ? "App display-details" : "App"}>
+      <Autocomplete onClickProduct={onClickProduct} />
+      <ProductDetail productId={productId} />
     </div>
   );
 }
 
-export default App;
+export default App
